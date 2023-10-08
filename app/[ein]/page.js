a3232f7135d644941e3d1dbda3076a761ea1afa5
Deej Cabral <deejc204@gmail.com>
2023-10-08 03:16:13 -0700

    added details and clickable links

---
+++ app/[ein]/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+
+export default function Company({ params }) {
+  const [companyData, setCompanyData] = useState(null);
+
+  useEffect(() => {
+    const fetchCompanyData = async () => {
+      try {
+        const res = await fetch(`/api/organizations/${params.ein}.json`);
+        if (!res.ok) {
+          throw new Error('Failed to fetch company data');
+        }
+        const companyDetails = await res.json();
+        setCompanyData(companyDetails.organization);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCompanyData();
+  }, [params.ein]);
+
+  return (
+    <div>
+      {companyData ? (
+        <div>
+          <h1>{companyData.name}</h1>
+          <p>City: {companyData.city}</p>
+          <p>State: {companyData.state}</p>
+          {/* Add more details as needed */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
