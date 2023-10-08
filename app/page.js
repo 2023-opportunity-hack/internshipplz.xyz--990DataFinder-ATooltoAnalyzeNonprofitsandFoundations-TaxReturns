7fc78388aa5d26db20e4bb55dde80c7d43b2fa05
kingorem <93293924+systachy@users.noreply.github.com>
2023-10-07 22:57:56 -0700

    HUGE COOL MAJOR FAILING UPDATE TO THE STYLING +POG100 +COOLDOGE

--- app/page.js
+++ app/page.js
@@ -1,20 +1,41 @@
-import styles from './page.module.css'
+// Update your Home component
 
+import styles from './page.module.css'
+import img1 from "./email.png"
+import img2 from "./github.png"
+import img3 from "./instagram.jpg"
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.buttonContainer}>
-      <div className={styles.button}>
-        <button>
-          Search for Organizations
-        </button>
+        <div className={styles.button}>
+          <button>
+            Search for Organizations
+          </button>
+        </div>
+        <div className={styles.button}>
+          <button>
+            Search for Non-Profit Salaries
+          </button>
+        </div>
       </div>
-      <div className={styles.button}>
-        <button>
-          Search for Non-Profit Salaries
-        </button>
-      </div>
-      </div>
+      <footer className={styles.footer}>
+        <div className={styles.footerButton}>
+          <button>
+            <img src={img2} alt=""/>
+          </button>
+        </div>
+        <div className={styles.footerButton}>
+          <button>
+            <img src={img1} alt=""/>
+          </button>
+        </div>
+        <div className={styles.footerButton}>
+          <button>
+            <img src={img3} alt=""/>
+          </button>
+        </div>
+      </footer>
     </main>
-  )
+    )
 }
