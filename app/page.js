8948eef5c6ae4679faceaf898fdaf39f64d3d46b
kingorem <93293924+systachy@users.noreply.github.com>
2023-10-08 00:21:17 -0700

    update newer orgSearch code with styling upgrades

--- app/page.js
+++ app/page.js
@@ -3,6 +3,11 @@
 import { useState } from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
+import Image from 'next/image'; // Import the next/image component
+
+import img1 from "./email.png"
+import img2 from "./github.png"
+import img3 from "./instagram.png"
 
 export default function Home() {
   return (
@@ -22,6 +27,23 @@ export default function Home() {
           </Link>
           </p>
       </div>
+      <footer className={styles.footer}>
+        <div className={styles.footerButton}>
+          <button>
+            <Image src={img2} alt=""/>
+          </button>
+        </div>
+        <div className={styles.footerButton}>
+          <button>
+            <Image src={img1} alt=""/>
+          </button>
+        </div>
+        <div className={styles.footerButton}>
+          <button>
+            <Image src={img3} alt=""/>
+          </button>
+        </div>
+      </footer>
     </main>
   )
 }
